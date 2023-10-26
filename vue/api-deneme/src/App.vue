0f4f3a6ed51b314ca0a20f9af83a127e8b501e05
TengriHan <furkanbayraktarr_@hotmail.com>
2023-10-26 12:03:05 +0300

    Dog API

---
+++ vue/api-deneme/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted, ref } from "vue"
+
+const data = ref([]);
+
+async function fetchData(name) {
+  const result = await fetch(`https://dog.ceo/api/breed/${name}/images`)
+    .then(data => data.json())
+    .then((response) => response.message);
+
+    return result;
+}
+
+onMounted(async () => {
+  data.value = await fetchData("pug");
+})
+</script>
+
+<template>
+  <img v-for="myData in data" :src="myData" :key="myData">
+</template>
+
+<style scoped>
+  img{
+    max-width: 150px;
+    max-height: 150px;
+    margin: 5px;
+    border: 2px solid gray;
+}
+</style>
